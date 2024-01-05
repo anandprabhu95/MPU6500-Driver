9413c3f601d569f8f8ddfbcee264d7b4ac99e195
anandprabhu95 <anandprabhu95@outlook.com>
2024-01-05 05:52:09 +0100

    Gyro Calibration

--- imu/imu.ino
+++ imu/imu.ino
@@ -2,6 +2,8 @@
 
 
 float RateRoll, RatePitch, RateYaw;
+float RateCalRoll, RateCalPitch, RateCalYaw;
+int RateCalTime;
 
 
 void gyro_signals(void) {
@@ -43,12 +45,34 @@ void setup() {
   Wire.begin();
   delay(250);
   Wire.beginTransmission(0x68); 
-  Wire.write(0x6B);
-  Wire.write(0x00);
+  Wire.write(0x6B); //PWR_MGMT_1 
+  Wire.write(0x00); //CLKSEL == 0 : Clock Source: Internal 20MHz oscillator
   Wire.endTransmission();
+
+  
+  // GyroScope Calibration
+  // Take measurements gor 2000 ms and sum the measurements
+  for (RateCalTime=0;
+         RateCalTime<2000; 
+         RateCalTime ++) {
+    gyro_signals();
+    RateCalRoll = RateCalRoll + RateRoll;
+    RateCalPitch = RateCalPitch + RatePitch;
+    RateCalYaw = RateCalYaw + RateYaw;
+    delay(1);
+  }
+  // Average the measurements to obtain calibration values
+  RateCalRoll/=2000;
+  RateCalPitch/=2000;
+  RateCalYaw/=2000;
 }
+
+
 void loop() {
   gyro_signals();
+  RateRoll = RateRoll - RateCalRoll;
+  RatePitch = RatePitch - RateCalPitch;
+  RateYaw = RateYaw - RateCalYaw;
   Serial.print("Roll rate [deg/s]= ");
   Serial.print(RateRoll);
   Serial.print(" Pitch Rate [deg/s]= ");
@@ -56,4 +80,5 @@ void loop() {
   Serial.print(" Yaw Rate [deg/s]= ");
   Serial.println(RateYaw);
   delay(50);
+
 }
