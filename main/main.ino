3255ea98d6785c4a5e59b048d34172566b6f64a6
anandprabhu95 <anandprabhu95@outlook.com>
2024-01-09 22:15:26 +0100

    Calculate Angles

--- main/main.ino
+++ main/main.ino
@@ -1,53 +1,45 @@
-#include "myGyroScope.hpp"
+#include "main_arduino.hpp"
+#include "imu.hpp"
 
-float RateCalRoll, RateCalPitch, RateCalYaw;
-int RateCalTime;
-
-unsigned long BaudRate = 57600;
-uint32_t ArdClock = 400000;
+static ImuCal bb;
 
 void setup() {
-  Serial.begin(BaudRate);
-  pinMode(13, OUTPUT);
-  digitalWrite(13, HIGH);
-  Wire.setClock(ArdClock);
-  Wire.begin();
-  delay(250);
-
-  // Select Clock Source
-  Wire.beginTransmission(I2C_ADD);
-  Wire.write(PWR_MGMT_1);
-  Wire.write(CLKSEL_20MHZ);
-  Wire.endTransmission();
-
-  // GyroScope Calibration
-  // Take measurements gor 2000 ms and sum the measurements
-  for (RateCalTime = 0;
-       RateCalTime < 2000;
-       RateCalTime++) {
-    gyro_signals();
-    RateCalRoll = RateCalRoll + RateRoll;
-    RateCalPitch = RateCalPitch + RatePitch;
-    RateCalYaw = RateCalYaw + RateYaw;
-    delay(1);
-  }
-  // Average the measurements to obtain calibration values
-  RateCalRoll /= 2000;
-  RateCalPitch /= 2000;
-  RateCalYaw /= 2000;
+  main_setup();
+  ImuCal bb = init_imu();
 }
 
-
 void loop() {
-  gyro_signals();
-  RateRoll = RateRoll - RateCalRoll;
-  RatePitch = RatePitch - RateCalPitch;
-  RateYaw = RateYaw - RateCalYaw;
-  Serial.print("Roll rate [deg/s]= ");
-  Serial.print(RateRoll);
-  Serial.print(" Pitch Rate [deg/s]= ");
-  Serial.print(RatePitch);
-  Serial.print(" Yaw Rate [deg/s]= ");
-  Serial.println(RateYaw);
-  delay(50);
+  Xyz gres = gyro_signals();
+  Xyz ares = acc_signals();
+
+//  gres.x = gres.x - zz.gyrocal.x;
+//  gres.y = gres.y - zz.gyrocal.y;
+//  gres.z = gres.z - zz.gyrocal.z;
+//
+//  ares.x = ares.x - zz.acccal.x;
+//  ares.y = ares.y - zz.acccal.y;
+//  ares.z = ares.z - zz.acccal.z;
+
+  //Serial.print(" Roll rate [deg/s]= ");
+  //Serial.print(gres.x);
+  //Serial.print(" Pitch Rate [deg/s]= ");
+  //Serial.print(gres.y);
+  //Serial.print(" Yaw Rate [deg/s]= ");
+  //Serial.print(gres.z);
+//
+  //Serial.print(" aX [g]= ");
+  //Serial.print(ares.x);
+  //Serial.print(" aY [g]= ");
+  //Serial.print(ares.y);
+  //Serial.print(" aZ [g]= ");
+  //Serial.print(ares.z);
+  //Serial.println(a);
+
+  Xyz thres = acc_angles(bb);
+  Serial.print(" aX [g]= ");
+  Serial.print(thres.x);
+  Serial.print(" aY [g]= ");
+  Serial.println(thres.y);
+
+  delay(sample_rate);
 }
---
+++ main/main_arduino.hpp
@@ -0,0 +1,10 @@
+#ifndef MAIN_ARDUINO_
+#define MAIN_ARDUINO_
+
+#include "Arduino.h"
+#include "mpu6500.h"
+
+// Arduino setup() and loop() will be defined here in the future
+void main_setup(void);
+
+#endif
